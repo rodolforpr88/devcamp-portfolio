6fd4a744ca23543ead938d72db8db9a2aea728bb
Rodolfo Porfirio Rodrigues <rodolforpr@localhost.localdomain>
2020-06-25 11:18:21 -0300

    Impllemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,10 +13,23 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        # format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
-        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
